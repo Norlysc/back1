121b5527def7041f594687f80c046ce62989ba3b
Sarahy <josefajg23@gmail.com>
2024-06-01 19:05:21 -0400

    Ajuste dashboard

--- project-root/routes/index.js
+++ project-root/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "e-Actividad 1.1" });
 });
 
 module.exports = router;
