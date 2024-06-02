816622cb9ad9d31f23557c81dd87037d309fdb7a
JP_Castro <127366413+JPC4stro@users.noreply.github.com>
2024-06-01 20:53:49 -0400

    modificacion de archivos

--- project-root/routes/users.js
+++ project-root/routes/users.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/UserControllers");
+router.use(express.json());
+
+
+router.get("/", UserController.getUsers);
+router.get("/:id", UserController.getUserById);
+
 
 router.post("/", UserController.addUser);
+router.post("/:id", UserController.addUserAccount);
+
+
 router.put("/:id", UserController.editUser);
+
+
 router.delete("/:id", UserController.deleteUser);
-router.get("/:id/accounts", UserController.getUserAccounts);
+router.delete("/:id/cuentas/:cuenta", UserController.deleteUserAccount);
+
 
 module.exports = router;
